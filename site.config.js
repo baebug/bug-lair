f998ed1fd7374ab272cf3584ab7b51af18fcb36c
baebug <ufr134@gmail.com>
2023-05-07 18:18:51 +0900

    edit site config

--- site.config.js
+++ site.config.js
@@ -1,32 +1,36 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "baebug",
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "backend developer",
+    bio: "이것저것 관심이 많은 사람",
+    email: "ufr134@gmail.com",
+    linkedin: "",
+    github: "baebug",
+    instagram: "baebug_",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `tonny-bunny`,
+      href: "https://github.com/baebug/tonny-bunny",
+    },
+    {
+      name: `HAST`,
+      href: "https://github.com/baebug/tonny-bunny",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "dev-lair",
+    description: "welcome to bug's Lair!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://baebug.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: ["ko-KR", "en-US"], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["Blog", "Website", "Notion"],
@@ -53,7 +57,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "baebug/dev-lair",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
